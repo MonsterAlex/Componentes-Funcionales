445a5b2c301969761f3c3c84fc7617aa0d961bd3
Alejandro Reyes <diego_1394_rg@hotmail.com>
2023-04-24 18:07:25 -0600

    Estilizando el formulario con Material UI

--- src/Components/FormSignUp.jsx
+++ src/Components/FormSignUp.jsx
@@ -1,18 +1,21 @@
 import Button from "@mui/material/Button"
+import TextField from "@mui/material/TextField"
+import Switch from "@mui/material/Switch"
+import FormGroup from "@mui/material/FormGroup"
+import FormControlLabel from "@mui/material/FormControlLabel"
 
 function FormSignUp()
 {
     return  <form>
-                <label>Nombre</label>
-                <input type="text" />
-                <label>Apellidos</label>
-                <input type="text" />
-                <label>Correo electronico</label>
-                <input type="email" />
-                <label>Promociones</label>
-                <input type="checkbox" />
-                <label>Novedades</label>
-                <input type="checkbox" />
+                <TextField id="name" label="Nombre" variant="outlined" fullWidth margin="normal" /> {/*FullWidth permite que tome el 100% del tamaño del componente padre (form)*/}
+                <TextField id="lastname" label="Apellidos" variant="outlined" fullWidth margin="normal" />
+                <TextField id="email" label="Email" variant="outlined" fullWidth margin="normal" />
+                
+                <FormGroup>
+                    <FormControlLabel control={ <Switch defaultChecked /> } label="Promociones" />
+                    <FormControlLabel control={ <Switch defaultChecked /> } label="Novedades" />
+                </FormGroup>
+
                 <Button variant="contained" >Registrarse</Button> {/*lo que se encuentra dentro de variant lo podemos encontrar en la documentacion de Material UI*/}
             </form>
 }
